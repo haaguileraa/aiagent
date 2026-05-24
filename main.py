7abed1794a68ea16b9a398a351e0aa433a11994c
hernan <haguil0122@gmail.com>
2026-05-25 00:38:38 +0200

    obtaining model's response and tokens used from user input

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from aiagent!")
+import argparse
+import os
+from dotenv import load_dotenv
+from google import genai
 
+
+def main() -> None: 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("Could not load API key")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description = "Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args() 
+    user_prompt = args.user_prompt
+    print(f"User prompt: {user_prompt}")
+ 
+    response = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = user_prompt)
+
+    
+    metadata = response.usage_metadata
+    prompt_tokens = metadata.prompt_token_count
+    response_tokens = metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
